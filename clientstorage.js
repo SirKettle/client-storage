fcdb465c07861f1785f045915083f70f5b1d5fee
Will Thirkettle <will@thekettlestudio.co.uk>
2013-03-15 15:46:37 +0000

    adding the code

---
+++ clientstorage.js
@@ -0,0 +1,116 @@
+
+/*
+ * Local Storage interface to store/retrieve information on the client
+ * TODO: use a cookie fallback - currently returns null for everything which does not support localStorage
+ */
+
+var ClientStorage = (function(){
+	
+	// member variables
+	var m_sNamespace = "cs_";
+	var m_bUseLocalStorage = false;
+	var m_bUseCookies = false;
+	
+	// cookie code from mdn - https://developer.mozilla.org/en-US/docs/DOM/document.cookie
+	var CookieHandler = {
+		getItem: function (sKey) {
+			if (!sKey || !this.hasItem(sKey)) { return null; }
+			return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+		},
+		setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+			if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return; }
+			var sExpires = "";
+			if (vEnd) {
+				switch (vEnd.constructor) {
+					case Number:
+						sExpires = vEnd === Infinity ? "; expires=Tue, 19 Jan 2038 03:14:07 GMT" : "; max-age=" + vEnd;
+						break;
+					case String:
+						sExpires = "; expires=" + vEnd;
+						break;
+					case Date:
+						sExpires = "; expires=" + vEnd.toGMTString();
+						break;
+				}
+			}
+			document.cookie = escape(sKey) + "=" + escape(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+		},
+		removeItem: function (sKey, sPath) {
+			if (!sKey || !this.hasItem(sKey)) { return; }
+			document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sPath ? "; path=" + sPath : "");
+		},
+		hasItem: function (sKey) {
+			return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+		}
+	};
+	
+	return {
+		Init: function(sNamespace, bCookiesFallback){
+			m_sNamespace = typeof sNamespace === "string" ? sNamespace : m_sNamespace;
+			m_bUseLocalStorage = typeof window.localStorage !== "undefined";
+			
+			if (!m_bUseLocalStorage)
+			{
+				m_bUseCookies = typeof bCookiesFallback == "boolean" ? bCookiesFallback : false;
+			}
+		},
+		Get: function(sKey){
+	
+			var Ret = null;
+			
+			if (typeof sKey === "string")
+			{
+				if (m_bUseLocalStorage)
+				{
+					Ret = window.localStorage.getItem(m_sNamespace + sKey);
+				}
+				else if (m_bUseCookies)
+				{
+					CookieHandler.getItem(m_sNamespace + sKey);
+				}
+
+				if (typeof Ret === "string")
+				{
+					var cValue = JSON.parse(Ret);
+
+					if (typeof cValue.Val !== "undefined")
+					{
+						Ret = cValue.Val;
+					}
+				}
+			}
+		
+			return Ret;
+		},
+		Set: function(sKey, Value){
+	
+			if (typeof sKey === "string" && typeof Value !== "undefined")
+			{
+				var sValue = JSON.stringify({ Val: Value });
+				
+				if (m_bUseLocalStorage)
+				{
+					window.localStorage.setItem(m_sNamespace + sKey, sValue);
+				}
+				else if (m_bUseCookies)
+				{
+					CookieHandler.setItem(m_sNamespace + sKey, sValue);
+				}
+			}
+		},
+		Remove: function(sKey){
+	
+			if (typeof sKey === "string")
+			{
+				if (m_bUseLocalStorage)
+				{
+					window.localStorage.removeItem(m_sNamespace + sKey);
+				}
+				else if (m_bUseCookies)
+				{
+					CookieHandler.removeItem(m_sNamespace + sKey);
+				}
+			}
+		}
+	};
+})();
